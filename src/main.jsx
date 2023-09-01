919cbe59e86579a56d6a205282315ec26e4c4095
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-31 23:48:32 -0400

    Added bulma import to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,5 @@ import ReactDOM from 'react-dom/client';
 // imports neseccary for routing
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+// imports bulma for styling
+import 'bulma/css/bulma.min.css';
