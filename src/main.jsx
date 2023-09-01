b3eb2b04b0dfaf9be2b0964ce17131172b01a2c5
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-31 23:50:29 -0400

    imported pages to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,3 +5,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 // imports bulma for styling
 import 'bulma/css/bulma.min.css';
+
+// import pages
+import AboutMe from './pages/AboutMe';
+import ContactMe from './pages/ContactMe';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
