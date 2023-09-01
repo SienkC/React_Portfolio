41214960be3a4144cda86be069b81607ff748f9a
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-09-01 01:23:52 -0400

    Added contact me page from prev portfolio

---
+++ src/pages/ContactMe.jsx
@@ -0,0 +1,29 @@
+export default function ContactMe() {
+    // returns Contact Me page
+    return (
+        <div className="container">
+            <ul className="level">
+                <li className="level-item has-text-centered">
+                    <p>Phone: </p>
+                    <a className="link" href="tel: 9549375623">954.937.5623</a>
+                </li>
+                <li className="level-item has-text-centered">
+                    <p>Email: </p>
+                    <a className="link" href="mailto: sienkiewichc@gmail.com">sienkiewichc@gmail.com</a>
+                </li>
+                <li className="level-item has-text-centered">
+                    <p>GitHub: </p>
+                    <a className="link" href="https://github.com/SienkC">SienkC</a>
+                </li>
+                <li className="level-item has-text-centered">
+                    <p>Twitter: </p>
+                    <a className="link" href="https://twitter.com/Court091997">Court091997</a>
+                </li>
+                <li className="level-item has-text-centered">
+                    <p>Spotify: </p>
+                    <a className="link" href="https://open.spotify.com/user/31wkyzldkx4ea2sm3ekaokzvys6a">Courtney S</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
