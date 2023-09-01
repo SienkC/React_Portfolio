90ff639562dec47719a150ddbff16e375afd0cb9
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-31 23:22:49 -0400

    Added server info to vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,3 +2,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+// server on port 3000 using react
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 3000,
+        open: true,
+    },
+});
