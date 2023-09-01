5be935a0a1f67f1bdc1a3c30ebed2fe27501e68f
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-09-01 00:40:33 -0400

    Added nav links to return statement and added export in nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,29 @@
 import { Link, useLocation } from 'react-router-dom';
 
-
+// function for nav items and links
 function Nav() {
     // get current page by looking at URL
     const currentPg = useLocation().pathname;
 
-    // assign each nav item its appropriate link
-    <ul className="tabs is-right is-large">
-        <li 
-            // If current page is AboutMe, then css will match
-            className={currentPg === "/" ? "is-active" : ""}>
-            <Link to="/">About Me</Link>
-        </li>
-        <li className={currentPg === "/ContactMe" ? "is-active" : ""}>
-            <Link to="/ContactMe">Contact Me</Link>
-        </li>
-        <li className={currentPg === "/Portfolio" ? "is-active" : ""}>
-            <Link to="/Portfolio">Portfolio</Link>
-        </li>
-        <li className={currentPg === "/Resume" ? "is-active" : ""}>
-            <Link to="/Resume">Resume</Link>
-        </li>
-    </ul>
+    return(
+        // assign each nav item its appropriate link
+        <ul className="tabs is-right is-large">
+            <li 
+                // If current page is AboutMe, then css will match
+                className={currentPg === "/" ? "is-active" : ""}>
+                <Link to="/">About Me</Link>
+            </li>
+            <li className={currentPg === "/ContactMe" ? "is-active" : ""}>
+                <Link to="/ContactMe">Contact Me</Link>
+            </li>
+            <li className={currentPg === "/Portfolio" ? "is-active" : ""}>
+                <Link to="/Portfolio">Portfolio</Link>
+            </li>
+            <li className={currentPg === "/Resume" ? "is-active" : ""}>
+                <Link to="/Resume">Resume</Link>
+            </li>
+        </ul>
+    );
 }
+
+export default Nav;
