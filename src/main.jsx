15961a77e5783deef3152785e235e67d80ece9f4
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-31 23:56:04 -0400

    Added paths for router and added error page

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,39 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // imports bulma for styling
 import 'bulma/css/bulma.min.css';
 
+import App from './App';
+
 // import pages
+import Error from './pages/Error';
 import AboutMe from './pages/AboutMe';
 import ContactMe from './pages/ContactMe';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
+
+// paths for router
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <AboutMe />
+            },
+            {
+                path: '/ContactMe',
+                element: <ContactMe />
+            },
+            {
+                path: '/Portfolio',
+                element: <Portfolio />
+            },
+            {
+                path: '/Resume',
+                element: <Resume />
+            }
+        ]
+    }
+]);
 
