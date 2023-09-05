240d8bbee02b7dde172c63fd6edc9b0dff7e98d5
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-09-05 18:16:54 -0400

    Removed prev contact and added state items for future form

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,29 +1,20 @@
+// import ability to use state for the form
+import { useState } from "react";
+
+function ContactForm(props) {
+    // set up input values as state elements
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [text, setText] = useState('');
+
+    
+}
+
 export default function ContactMe() {
-    // returns Contact Me page
+    // return form for Contact Me page
     return (
-        <div className="container">
-            <ul className="level">
-                <li className="level-item has-text-centered">
-                    <p>Phone: </p>
-                    <a className="link" href="tel: 9549375623">954.937.5623</a>
-                </li>
-                <li className="level-item has-text-centered">
-                    <p>Email: </p>
-                    <a className="link" href="mailto: sienkiewichc@gmail.com">sienkiewichc@gmail.com</a>
-                </li>
-                <li className="level-item has-text-centered">
-                    <p>GitHub: </p>
-                    <a className="link" href="https://github.com/SienkC">SienkC</a>
-                </li>
-                <li className="level-item has-text-centered">
-                    <p>Twitter: </p>
-                    <a className="link" href="https://twitter.com/Court091997">Court091997</a>
-                </li>
-                <li className="level-item has-text-centered">
-                    <p>Spotify: </p>
-                    <a className="link" href="https://open.spotify.com/user/31wkyzldkx4ea2sm3ekaokzvys6a">Courtney S</a>
-                </li>
-            </ul>
+        <div>
+
         </div>
     );
 }
