75e38d64e9da17730280bee81f35cb0571a3353a
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-09-05 17:37:35 -0400

    Added resume pdf in iframe

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,6 @@
+export default function Resume() {
+    // return Resume file
+    <div>
+        <iframe src="../assets/docs/Sienkiewich_Resume.pdf" width="100%" height="500px"></iframe>
+    </div>
+}
