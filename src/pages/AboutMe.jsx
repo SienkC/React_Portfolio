81b7062cba1834ab0c58802e2ad817158f128b77
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-09-05 17:54:33 -0400

    Moved assets to where they can be accessed

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
     // returns content for About Me page
     return (
         <div className="container">
-            <img src="../assets/images/Avatar.jpg" alt="Avatar to represent Courtney" style="float: right; margin: 5px;"></img>
+            <img src="../assets/images/Avatar.jpg" alt="Avatar to represent Courtney"></img>
             <p>
                         Hello, I am Courtney Sienkiewich, a developing developer currently based in Orlando, FL. 
                         At the moment I am going through a course with UCF Bootcamps to grow in full stack development.
