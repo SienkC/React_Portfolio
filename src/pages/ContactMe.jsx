3d0afa2c155b731aefb695e4af570355fdad025c
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-09-05 20:31:57 -0400

    Moved validation for name to onchange function

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -7,31 +7,34 @@ function ContactForm(props) {
     const [email, setEmail] = useState('');
     const [text, setText] = useState('');
 
-    
+    // // to prevent validation on first load
+    // useEffect(() => {
+    //     document.getElementById('name').style.borderColor = '';
+    // }, []);
 
-    // validate inputs
-    useEffect(() => {
-        console.log(name);
+    // // validate inputs
+    // useEffect(() => {
+    //     console.log(name);
 
-        if(name == '') {
-            const error = document.createElement('div');
-            // give id of error
-            error.setAttribute('id', 'error');
+    //     if(name == '') {
+    //         const error = document.createElement('div');
+    //         // give id of error
+    //         error.setAttribute('id', 'error');
 
-            // warning for user
-            error.innerHTML = 'Fill in the highlighted fields';
-            error.style.color = 'red';
-            document.getElementById('name').prepend(error);
-            document.getElementById('name').style.borderColor = 'red';
-        }
-        else {
-            const error = document.getElementById('error');
-            if (error) {
-                error.remove();
-            }
-            document.getElementById('name').style.borderColor = '';
-        }
-    });
+    //         // warning for user
+    //         error.innerHTML = 'Fill in the highlighted fields';
+    //         error.style.color = 'red';
+    //         document.getElementById('name').prepend(error);
+    //         document.getElementById('name').style.borderColor = 'red';
+    //     }
+    //     else {
+    //         const error = document.getElementById('error');
+    //         if (error) {
+    //             error.remove();
+    //         }
+    //         document.getElementById('name').style.borderColor = '';
+    //     }
+    // });
 
     // function for submit button
     const handleSubmit = (e) => {
@@ -47,25 +50,57 @@ function ContactForm(props) {
         setText('');
     };
 
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        if (value === '') {
+            const error = document.createElement('div');
+            // give id of error
+            error.setAttribute('id', 'error');
+
+            // warning for user
+            error.innerHTML = 'Fill in the highlighted fields';
+            error.style.color = 'red';
+            // e.target.prepend(error);
+            e.target.previousElementSibling.innerHTML = 'Enter your name!';
+            e.target.style.borderColor = 'red';
+            // e.target.placeholder = 'Fill in the field before submitting!'
+        }
+        else {
+            
+        }
+
+        if (name === 'name') {
+            setName(value);
+        }
+        return;
+    }
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
+                <label id="nameWarn"></label>
                 <input id="name"
                     type="text"
+                    name="name"
                     placeholder="Enter Your Name"
                     value={name}
-                    onChange={(e) => setName(e.target.value)}
+                    onChange={handleInputChange}
                 >
                 </input>
+                <label id="emailWarn"></label>
                 <input id="email"
                     type="text"
+                    name="email"
                     placeholder="Enter Your Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 >
                 </input>
+                <label id="textWarn"></label>
                 <textarea id="text"
                     type="text"
+                    name="text"
                     placeholder="Enter Your Comment"
                     value={text}
                     onChange={(e) => setText(e.target.value)}
