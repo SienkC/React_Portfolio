2e878c066b547dd5c8f52aa977edb3ada597f54c
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-09-05 23:02:30 -0400

    Added name to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,22 +7,25 @@ function Nav() {
 
     return (
         // assign each nav item its appropriate link
-        <ul className="tabs is-boxed is-right is-large navbar is-fixed-top">
-            <li 
-                // If current page is AboutMe, then css will match
-                className={currentPg === "/" ? "is-active" : ""}>
-                <Link to="/">About Me</Link>
-            </li>
-            <li className={currentPg === "/ContactMe" ? "is-active" : ""}>
-                <Link to="/ContactMe">Contact Me</Link>
-            </li>
-            <li className={currentPg === "/Portfolio" ? "is-active" : ""}>
-                <Link to="/Portfolio">Portfolio</Link>
-            </li>
-            <li className={currentPg === "/Resume" ? "is-active" : ""}>
-                <Link to="/Resume">Resume</Link>
-            </li>
-        </ul>
+        <div className="navbar is-fixed-top">
+            <h1 className='title navbar-item pb-0'>Courtney Sienkiewich</h1>
+            <ul className="navbar-item pb-0 navbar-end tabs is-boxed is-right is-large">
+                <li 
+                    // If current page is AboutMe, then css will match
+                    className={currentPg === "/" ? "is-active" : ""}>
+                    <Link to="/">About Me</Link>
+                </li>
+                <li className={currentPg === "/ContactMe" ? "is-active" : ""}>
+                    <Link to="/ContactMe">Contact Me</Link>
+                </li>
+                <li className={currentPg === "/Portfolio" ? "is-active" : ""}>
+                    <Link to="/Portfolio">Portfolio</Link>
+                </li>
+                <li className={currentPg === "/Resume" ? "is-active" : ""}>
+                    <Link to="/Resume">Resume</Link>
+                </li>
+            </ul>
+        </div>
     );
 }
 
