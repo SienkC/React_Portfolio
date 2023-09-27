bffbe349dd5ac4feecdfba201bed485009504651
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-09-27 01:06:14 -0400

    Added ability to have text appear over image

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,12 +12,24 @@ const Work = ({
         //             <p className="has-text-right"><a className="m-2 has-text-info-light is-size-5" href={item.repo}>View Repository on GitHub</a></p>
         //         </div>
         // </div>
-        <div class="card">
-            <div class="card-content">
-            <img className="image" style={{width: "100%", height: "50vh"}} src={"../assets/images/" + item.image}></img>
-                <div class="content">
-                    <p class="hidden-content">This content will be shown on hover.</p>
-                </div>
+
+        // <div class="card">
+        //     <div class="card-content">
+        //     <img className="image" style={{width: "100%", height: "50vh"}} src={"../assets/images/" + item.image}></img>
+        //         <div class="content">
+        //             <p class="hidden-content">This content will be shown on hover.</p>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className="card" >
+            <div className="card-image card-content">
+                <figure className="image">
+                    <img className="image" style={{width: "100%", height: "50vh"}} src={"../assets/images/" + item.image}></img>
+                </figure>
+            </div>
+            <div className="card-content is-overlay hidden-content">
+                <span className="tag is-primary ">Image label on overlay</span>
             </div>
         </div>
     );
