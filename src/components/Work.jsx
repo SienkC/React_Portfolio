769b17ca865e702cb6ab76eb6c5941bf4895c297
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-09-27 00:59:28 -0400

    Added hidden show content on hover

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,16 +1,24 @@
 const Work = ({
     item
-}) => { 
+}) => {
     return (
-        <div className="box has-background-info-dark column is-9">
-                <div className="workLabel">
-                    <h4 className="has-text-centered has-text-info-light is-size-1">{item.name}</h4>
-                    <p className="has-text-centered has-text-info-light is-size-4">{item.tech}</p>
-                    <a href={item.deployed}>
-                        <img className="image" style={{width: "100%", height: "50vh"}} src={"../assets/images/" + item.image}></img>
-                    </a>
-                    <p className="has-text-right"><a className="m-2 has-text-info-light is-size-5" href={item.repo}>View Repository on GitHub</a></p>
+        // <div className="box has-background-info-dark column is-9">
+        //         <div className="workLabel">
+        //             <h4 className="has-text-centered has-text-info-light is-size-1">{item.name}</h4>
+        //             <p className="has-text-centered has-text-info-light is-size-4">{item.tech}</p>
+        //             <a href={item.deployed}>
+        //                 <img className="image" style={{width: "100%", height: "50vh"}} src={"../assets/images/" + item.image}></img>
+        //             </a>
+        //             <p className="has-text-right"><a className="m-2 has-text-info-light is-size-5" href={item.repo}>View Repository on GitHub</a></p>
+        //         </div>
+        // </div>
+        <div class="card">
+            <div class="card-content">
+            <img className="image" style={{width: "100%", height: "50vh"}} src={"../assets/images/" + item.image}></img>
+                <div class="content">
+                    <p class="hidden-content">This content will be shown on hover.</p>
                 </div>
+            </div>
         </div>
     );
 }
