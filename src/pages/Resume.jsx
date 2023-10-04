3a9a9a45340b35da56265cbef90ae4570f4ac472
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-10-04 14:45:05 -0400

    Updated resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,7 +2,7 @@ export default function Resume() {
     // return Resume file
     return (
         <div className="container box has-background-info-light">
-            <iframe src="../assets/docs/Sienkiewich_Resume.pdf" width="100%" height="680px"></iframe>
+            <iframe src="../assets/docs/Resume.pdf" width="100%" height="680px"></iframe>
         </div>
     );
 }
